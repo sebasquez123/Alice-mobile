3ff6029e2318478475bfbf6e26ef9cce4f4a2562
SEBASQZ <sebastian.vasquez@utp.edu.co>
2026-01-14 23:47:22 -0500

    Prepared firebase credentials
    - Google credentials set up at android and ios projects respectively.
    - Firebase console project filled with local reliable fingerprints.
    - Project config level increased for logging and home initialization.

--- lib/features/home/view/home.dart
+++ lib/features/home/view/home.dart
@@ -1,4 +1,12 @@
+import 'package:alice/config.dart';
+import 'package:alice/features/home/domain/bloc/home.bloc.dart';
+import 'package:alice/features/home/domain/data/home.state.dart';
+import 'package:alice/features/home/domain/events/home.events.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+final logger = LoggerConfig(instanceName: 'Home');
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,16 +18,38 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) => Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.onSecondary,
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Alice'),
       ),
       body: LayoutBuilder(
         builder: (context, constraints) => Center(
-            child: Text(
-              'Welcome to Home',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          child: Column(
+            children: [
+              BlocConsumer<FactionBloc, FactionState>(
+                listener: (context, state) {
+                  logger.critical('Faction state changed: ${state.toString()}');
+                },
+                builder: (context, state) => Column(
+                    children: [
+                      Text(
+                        state.faction,
+                        style: const TextStyle(fontSize: 32),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => context.read<FactionBloc>().add(ToggleFaction()),
+                        child: const Text('Toggle Faction'),
+                      ),
+                    ],
+                  ),
+              ),
+              Text(
+                'Welcome to Alice!',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
           ),
+        ),
       ),
     );
 }
--- lib/navigator.dart
+++ lib/navigator.dart
@@ -9,8 +9,7 @@ import 'package:alice/features/settings/view/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-final logger = LoggerConfig().print;
-
+final logger = LoggerConfig(instanceName: 'Navigator');
 
 class Routes {
   static const String home = '/';
@@ -74,7 +73,7 @@ class _AppNavigator extends StatelessWidget {
   const _AppNavigator({
     required this.child,
   });
-
+  
   @override
   Widget build(BuildContext context) => PopScope(
         canPop: false,
@@ -93,21 +92,25 @@ Future<void> leaveAppFromNative(BuildContext context, bool didpop) async {
     final currentPath = GoRouter.of(context).state.name;
     switch (currentPath) {
       case Routes.signIn:
-        context.replaceNamed(Routes.signIn);
-
+        logger.info('Walking from sign-in to home');
+        context.replaceNamed(Routes.home);
+        break;
       case Routes.home:
         exit(0);
 
       case Routes.quotation:
+        logger.info('Walking from quotation screen to home');
         context.replaceNamed(Routes.home);
         break;
 
       case Routes.settings:
-      
+        logger.info('Walking from settings to home');
         context.replaceNamed(Routes.home);
         break;
 
       default:
-        exit(0);
+        logger.warning('No path found, returning to home');
+        context.replaceNamed(Routes.home);
+        break;
     }
   }
